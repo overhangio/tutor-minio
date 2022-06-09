ac90d21e7645f6a243099e86f39bbabc518903c5
Jhony Avella <jhony.avella@edunext.co>
2022-06-09 14:48:36 -0500

    v14.0.0: upgrade to Nutmeg
    * chore: first plugin upgrade changes.
    Based on instructions at https://discuss.overhang.io/t/how-to-upgrade-a-tutor-plugin/1488
    
    * chore: Migrating from V0 to V1 plugin
    
    * fix: formatting openedx-auth patch as YAML patch (https://discuss.overhang.io/t/how-to-upgrade-a-tutor-plugin/1488/5). Adding new MinIO server and MinIO client image versions in the Readme file

--- tutorminio/plugin.py
+++ tutorminio/plugin.py
@@ -1,17 +1,20 @@
 import os
 from glob import glob
 
-from .__about__ import __version__
+import pkg_resources
 
+from tutor import hooks as tutor_hooks
+
+from .__about__ import __version__
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 config = {
-    "set": {
+    "overrides": {
         "OPENEDX_AWS_ACCESS_KEY": "openedx",
         "OPENEDX_AWS_SECRET_ACCESS_KEY": "{{ MINIO_AWS_SECRET_ACCESS_KEY }}",
     },
-    "add": {
+    "unique": {
         "AWS_SECRET_ACCESS_KEY": "{{ 24|random_string }}",
     },
     "defaults": {
@@ -21,24 +24,48 @@ config = {
         "VIDEO_UPLOAD_BUCKET_NAME": "openedxvideos",
         "HOST": "files.{{ LMS_HOST }}",
         "CONSOLE_HOST": "minio.{{ LMS_HOST }}",
-        "DOCKER_IMAGE": "docker.io/minio/minio:RELEASE.2021-11-09T03-21-45Z",
-        "MC_DOCKER_IMAGE": "docker.io/minio/mc:RELEASE.2021-11-16T20-37-36Z",
+        "DOCKER_IMAGE": "docker.io/minio/minio:RELEASE.2022-05-08T23-50-31Z",
+        "MC_DOCKER_IMAGE": "docker.io/minio/mc:RELEASE.2022-05-09T04-08-26Z",
         "GATEWAY": None,
     },
 }
 
-templates = os.path.join(HERE, "templates")
+tutor_hooks.Filters.COMMANDS_PRE_INIT.add_item((
+    "minio",
+    ("minio", "tasks", "minio", "pre-init"),
+))
 
-hooks = {
-    "pre-init": ["minio"],
-}
-
-
-def patches():
-    all_patches = {}
-    for path in glob(os.path.join(HERE, "patches", "*")):
-        with open(path) as patch_file:
-            name = os.path.basename(path)
-            content = patch_file.read()
-            all_patches[name] = content
-    return all_patches
+# Add the "templates" folder as a template root
+tutor_hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
+    pkg_resources.resource_filename("tutorminio", "templates")
+)
+# Render the "build" and "apps" folders
+tutor_hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
+    [
+        ("minio/build", "plugins"),
+        ("minio/apps", "plugins"),
+    ],
+)
+# Load patches from files
+for path in glob(
+    os.path.join(
+        pkg_resources.resource_filename("tutorminio", "patches"),
+        "*",
+    )
+):
+    with open(path, encoding="utf-8") as patch_file:
+        tutor_hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
+# Add configuration entries
+tutor_hooks.Filters.CONFIG_DEFAULTS.add_items(
+    [
+        (f"MINIO_{key}", value)
+        for key, value in config.get("defaults", {}).items()
+    ]
+)
+tutor_hooks.Filters.CONFIG_UNIQUE.add_items(
+    [
+        (f"MINIO_{key}", value)
+        for key, value in config.get("unique", {}).items()
+    ]
+)
+tutor_hooks.Filters.CONFIG_OVERRIDES.add_items(list(config.get("overrides", {}).items()))
