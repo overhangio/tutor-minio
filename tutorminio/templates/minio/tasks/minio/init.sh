846e12b3db66dbe3d669f631f350cf85a9369104
henrrypg <henrry.pulgarin@edunext.co>
2023-08-22 16:43:52 -0500

    feat: add separated grades bucket and manage querystring by config var

--- tutorminio/templates/minio/tasks/minio/init.sh
+++ tutorminio/templates/minio/tasks/minio/init.sh
@@ -1,5 +1,5 @@
 mc config host add minio http://minio:9000 {{ OPENEDX_AWS_ACCESS_KEY }} {{ OPENEDX_AWS_SECRET_ACCESS_KEY }} --api s3v4
-mc mb --ignore-existing minio/{{ MINIO_BUCKET_NAME }} minio/{{ MINIO_FILE_UPLOAD_BUCKET_NAME }} minio/{{ MINIO_VIDEO_UPLOAD_BUCKET_NAME }}
+mc mb --ignore-existing minio/{{ MINIO_BUCKET_NAME }} minio/{{ MINIO_FILE_UPLOAD_BUCKET_NAME }} minio/{{ MINIO_VIDEO_UPLOAD_BUCKET_NAME }} minio/{{ MINIO_GRADES_BUCKET_NAME }}
 
 {% if MINIO_GATEWAY != "azure" %}
 # Make common file upload bucket public (e.g: for forum image upload)
