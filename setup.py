69fb65dac32cadbc058f191b0eb678488a83eac2
Régis Behmo <regis@behmo.com>
2023-11-20 17:39:47 +0100

    feat: simplify nightly version management
    By pulling the version suffix from tutor, we avoid git conflicts when
    merging the release branch in nightly.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with io.open(
 
 setup(
     name="tutor-minio",
-    version=about["__package_version__"],
+    version=about["__version__"],
     url="https://docs.tutor.overhang.io/",
     project_urls={
         "Documentation": "https://docs.tutor.overhang.io/",
